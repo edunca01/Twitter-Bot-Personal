2e88f7f508e7666fb1883838f7efe662afd189c9
Eugen Dunca <edunca01@outlook.com>
2022-10-19 23:56:22 -0400

    Converts tweets into .csv file

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import sys
 import multiprocessing
-
+import pandas as pd
 from botutils import config
 from botutils import fetch_data
 
@@ -8,9 +8,15 @@ if __name__ == '__main__':
     print('Starting Twitter Bot')
     api = config.get_api()
     print('Finished Initialization')
+    public_tweets = api.home_timeline()
 
     #dataframe
-    column = [Tweet, Time]
+    column = ['Tweet', 'Time']
+    data = []
 
-    #public_tweets = api.home_timeline()
+    for tweet in public_tweets:
+        data.append([tweet.text, tweet.created_at])
+
+    df = pd.DataFrame(data,columns=column)
+    df.to_csv('tweets.csv')
     #print(public_tweets)
