ede53f5587242d810665098537c12b582b2e7736
Eugen Dunca <edunca01@outlook.com>
2022-10-26 02:40:51 -0400

    Added foundation for mongodb + hid keys

--- botutils/config.py
+++ botutils/config.py
@@ -1,22 +1,20 @@
 import tweepy
-from . import utils
-
-logger = utils.get_logger()
+import os
 
 def get_api():
     try:
-        __API_KEY = "jall74fWCw9VdxBKM728MPqHx"
-        __API_SECRET_KEY = "vZk2NiS3WyRj5CsGntOSFlEe0rRv3XytwfZjAMh0WUbcELOY0A"
-        __ACCESS_TOKEN = "1582154646789001217-0AJheDFr3f66BxVsBl6yEmo11JjaPD"
-        __ACCESS_TOKEN_SECRET = "phz1z8parmx9FawTnwAKmaGni0k9zmD8fqPIeWheuPBIr"
+        __API_KEY = os.environ['API_KEY']
+        __API_SECRET_KEY = os.environ['API_SECRET_KEY']
+        __ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
+        __ACCESS_TOKEN_SECRET = os.environ['ACCESS_TOKEN_SECRET']
 
         auth = tweepy.OAuthHandler(__API_KEY, __API_SECRET_KEY)
         auth.set_access_token(__ACCESS_TOKEN, __ACCESS_TOKEN_SECRET)
         api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
         api.verify_credentials()
-        logger.info('Authentication successful')
-    except KeyError:
-        logger.error('Twitter credentials not supplied.')
+        print('Authentication successful')
+    except KeyError as e:
+        print('Twitter credentials not supplied.',e)
     except Exception as e:
-        logger.error(e)
+        print(e)
     return api
